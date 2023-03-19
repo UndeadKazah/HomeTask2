95a1cd33056b76c21bd5f9a22185fc2a8273fc2b
unknown <tarantylhomie@gmail.com>
2023-03-19 17:52:34 +0300

    Task 10 done

---
+++ Task_10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число.\n Для выхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
